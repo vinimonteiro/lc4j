8ce8393509d39ef2092fb8feaa8c8f3b743df35e
Vinicius Monteiro <vinidsmonteiro@gmail.com>
2025-12-17 11:17:11 +0000

    simple chat

---
+++ lc4j-simple-chat/src/main/java/Main.java
@@ -0,0 +1,17 @@
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+public class Main {
+
+    public static void main(String[] args){
+        final ChatModel model = OpenAiChatModel.builder()
+                .baseUrl("http://langchain4j.dev/demo/openai/v1")
+                .apiKey("demo")
+                .modelName("gpt-4o-mini")
+                .build();
+
+        final String answer = model.chat("Say 'Hello World' in portuguese");
+        System.out.println(answer); // "Olá Mundo"
+    }
+
+}
